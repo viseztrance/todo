0fb1aff39c3f6099b91ec5c73034daec5d15bb2d
Daniel Mircea <daniel@viseztrance.com>
2015-01-24 17:35:55 +0200

    Add status and color to entries

--- src/todo/entry.rs
+++ src/todo/entry.rs
@@ -1,13 +1,28 @@
+use regex::Regex;
+
 pub struct Entry {
     pub id: isize,
-    pub content: String
+    pub content: String,
+    pub status: String,
+    pub color: Option<String>
 }
 
 impl Entry {
-    pub fn new(id: isize, content: String) -> Entry {
+    pub fn new(id: isize, data: String) -> Entry {
+        let expr    = regex!(r"(^\[(\w+)\]? (.*?) ?#?([:xdigit:]+)?$)?");
+        let capture = expr.captures(data.as_slice()).unwrap();
+        let status  = capture.at(2).unwrap_or("PENDING").to_string();
+        let content = capture.at(3).unwrap_or(data.as_slice()).to_string();
+        let color   = match capture.at(4) {
+            Some(val) => Some(val.to_string()),
+            None => None
+        };
+
         Entry {
             id: id,
-            content: content
+            content: content,
+            status: status,
+            color: color
         }
     }
 }
